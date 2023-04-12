414a48efdd1f805f64a9299a45bef5803ce33b63
Yanis <yanis.vroland@gmail.com>
2023-04-12 11:35:50 +0200

    Import firebase

--- app/src/main/java/com/mvince/compose/ui/components/CustomOutlinedTextField.kt
+++ app/src/main/java/com/mvince/compose/ui/components/CustomOutlinedTextField.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -16,6 +17,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,6 +69,12 @@ fun CustomOutlinedTextField(
             visualTransformation = visualTransformation,
             leadingIcon = leadingIcon,
             trailingIcon = trailingIcon,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrect = false,
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
             //isErrorValue = isErrorValue,
             modifier = Modifier
                 .fillMaxWidth()
---
+++ app/src/main/java/com/mvince/compose/ui/signup/SignupScreen.kt
@@ -0,0 +1,122 @@
+package com.mvince.compose.ui.signup
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.mvince.compose.ui.components.CustomButton
+import com.mvince.compose.ui.components.CustomOutlinedTextField
+import com.mvince.compose.R
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen(navController: NavHostController) {
+    val viewModel = hiltViewModel<SignupViewModel>()
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var password2 by remember { mutableStateOf("") }
+    var showPassword by remember { mutableStateOf(false) }
+    var showPassword2 by remember { mutableStateOf(false) }
+
+    val authResource = viewModel.signupFlow.collectAsState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Test APP") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+                    }
+                })
+        },
+    ) {
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "Inscription", style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.SansSerif
+                )
+            )
+            Spacer(modifier = Modifier.height(64.dp))
+
+            CustomOutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(text = "Mail") },
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            CustomOutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = "Mot de passe") },
+                visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconButton(
+                        onClick = { showPassword = !showPassword }
+                    ) {
+                        Icon(
+                            painter = if (showPassword) painterResource(id = R.drawable.visibility) else painterResource(
+                                id = R.drawable.visibility_off
+                            ),
+                            contentDescription = if (showPassword) "Hide password" else "Show password",
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+            CustomOutlinedTextField(
+                value = password,
+                onValueChange = { password2 = it },
+                label = { Text(text = "Mot de passe ") },
+                visualTransformation = if (showPassword2) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconButton(
+                        onClick = { showPassword2 = !showPassword2 }
+                    ) {
+                        Icon(
+                            painter = if (showPassword2) painterResource(id = R.drawable.visibility) else painterResource(
+                                id = R.drawable.visibility_off
+                            ),
+                            contentDescription = if (showPassword2) "Hide password" else "Show password",
+                        )
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(64.dp))
+            CustomButton(
+                title = "Valider",
+                onClick = { viewModel.signupUser(email, password) },
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
